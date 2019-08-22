07015aefd1d4da784edf28f3f0d1e1831e94e2bd
nstryder <parasyte217@gmail.com>
2019-08-22 16:30:54 -0700

    First commit

---
+++ equipmentList.js
@@ -0,0 +1,70 @@
+export default [
+    {
+        name: "Aphex 320A Compellor",
+        description: "2 Channel compressor/limiter",
+        image: "equipment_img/aphex320a.jpg",
+        stock: 2,
+        price: 595.00
+    },
+    {
+        name: "Aphex 320D Compellor",
+        description: "2 Channel compressor/limiter",
+        image: "equipment_img/aphex-320d.jpg",
+        stock: 1,
+        newPrice: 1699.00,
+        price: 795.00
+    },
+    {
+        name: "Audioarts D-75",
+        description: "Complete Console 6ft cable and punch block set",
+        stock: 1,
+        price: 499.00
+    },
+    {
+        name: "Burk Arc Plus",
+        description: "Transmitter Remote Control TCP/IP Based Platform with " +
+            "Built-in web-interface. Includes PLUSXICRU integrated IP-based " +
+            "command relay unit.",
+        image: "equipment_img/burk_ip.jpg",
+        stock: 1,
+        newPrice: 3990.00,
+        price: 1795.00,
+    },
+    {
+        name: "Dayton Industrial AFC3",
+        description: "Rackmount EAS 3 receiver chassis with FM, FM, weather cards," +
+            "or FM, AM, weather cards installed",
+        image: "equipment_img/dayton1_afc3.jpg",
+        stock: 1,
+        newPrice: 1389.00,
+        price: 495.00,
+    },
+    {
+        name: "DBX 168A",
+        description: "Studio Dynamics Processor",
+        image: "equipment_img/dbx168a.jpg",
+        stock: 1,
+        price: 249.00,
+    },
+    {
+        name: 'Inovonics 716 "David II"',
+        description: "FM Audio Processor with built-int stereo generator",
+        image: "equipment_img/inov_davidii.jpg",
+        stock: 2,
+        price: 795.00,
+    },
+    {
+        name: "Inovonics 531",
+        description: "Freq. Agile FM modulation monitor with LED readout",
+        image: "equipment_img/inovonics_531_fm.jpg",
+        stock: 1,
+        price: 1295.00,
+    },
+    {
+        name: "Inovonics 530",
+        description: "Freq. Agile FM modulation monitor",
+        image: "equipment_img/inov_530.1.jpg",
+        stock: 2,
+        price: 1095.00,
+    }
+];
